3c75f22f873930b126ac60596156841b083ca17b
novatogg52 <167790781+novatogg52@users.noreply.github.com>
2024-09-20 11:34:40 -0300

    Add files via upload

---
+++ super/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'quando foi o inicio da segunda guerra mundial e quando foi o seu termino?',
+    '1 de setembro de 1939 – 2 de setembro de 1945'
+)
+
+criaCartao(
+    'historia',
+    ' Quando foi assinado o Tratado de Tordesilhas? ',
+    'foi em 1494'
+)
+
+criaCartao(
+    'historia',
+    'Que filósofo falou: "Se penso, logo existo"?',
+    'foi Rene descarte'
+)
+
+criaCartao(
+    'historia',
+    ' De qual mitologia são esses deuses: Saturno, Júpiter, Juno, Plutão, Netuno, Vesta, Ceres e Febo? ',
+    'mitologia romana'
+)
